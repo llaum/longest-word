c7a82eba7b3cbee086455b8c215781c5318e2ba9
Guillaume Allard <guillaume.allard@gmail.com>
2019-06-18 16:16:13 +0200

    Update is_valid method with the solution

--- game.py
+++ game.py
@@ -8,8 +8,12 @@ class Game():
     def __init__(self):
         self.grid = [random.choice(string.ascii_uppercase) for i in range(0, 9)]
     def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy() # Consume letters from the grid
         for letter in word:
-            if letter not in self.grid:
+            if letter in letters:
+                letters.remove(letter)
+            else:
                 return False
         return True
-
