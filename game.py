551ba2aa43513d7169a7455a69573d9c06205393
Guillaume Allard <guillaume.allard@gmail.com>
2019-06-18 15:51:49 +0200

    Add is_valid tests and method

--- game.py
+++ game.py
@@ -1,6 +1,15 @@
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
 import random
 import string
 
 class Game():
     def __init__(self):
         self.grid = [random.choice(string.ascii_uppercase) for i in range(0, 9)]
+    def is_valid(self, word):
+        for letter in word:
+            if letter not in self.grid:
+                return False
+        return True
+
--- tests/test_game.py
+++ tests/test_game.py
@@ -10,3 +10,13 @@ class TestGame(unittest.TestCase):
         self.assertEqual(len(grid), 9)
         for letter in grid:
             self.assertIn(letter, string.ascii_uppercase)
+
+    def test_digits_is_invalid(self):
+        new_game = Game()
+        grid = new_game.grid
+        self.assertFalse(new_game.is_valid("123"))
+
+    def test_grid_is_valid(self):
+        new_game = Game()
+        grid = new_game.grid
+        self.assertTrue(new_game.is_valid(grid))
